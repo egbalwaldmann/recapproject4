b4074ac79fd8a3a98a903ae666b3de2fc47d7a9e
egbalwaldmann <egbal@waldmann.dev>
2023-02-14 11:28:36 +0100

    creates basic form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div> Hello World</div>;
+  return (
+    <div>
+      <form>
+        <h1>weather app</h1>
+
+        {/* new activity field */}
+        <h2>Add new activity</h2>
+        <label for="activity-name">activity name</label>
+        <input id="activity-name" type="text" name="activity-name"></input>
+
+        {/* good weather activity checkbox */}
+        <label for="good-weather-activity">Good-weather activity</label>
+        <input
+          id="good-weather-activity"
+          type="checkbox"
+          name="good-weather-activity"
+        ></input>
+
+        {/* submit button */}
+        <input type="submit" value="Submit"></input>
+      </form>
+    </div>
+  );
 }
 
 export default App;
